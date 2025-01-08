e9b4c852674d91739e83db770195a76cf5ac4df7
dem123456789 <diao_em@hotmail.com>
2025-01-08 17:48:41 +0800

    add flow matching

--- src/model/formulation/diffusion.py
+++ src/model/formulation/diffusion.py
@@ -50,7 +50,6 @@ def get_alphas_sigmas(t):
     """Returns the scaling factors for the clean image (alpha) and for the
     noise (sigma), given a timestep."""
     return torch.cos(t * math.pi / 2), torch.sin(t * math.pi / 2)
-    # return 1 - t, t
 
 
 def diffusion(core, cfg):
